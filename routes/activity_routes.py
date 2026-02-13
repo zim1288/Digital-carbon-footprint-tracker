cd453bc2117bff480dcc63e3e15c21b224ef3f1a
Naisha1211 <Khan2305341211@diu.edu.bd>
2026-02-13 22:53:27 +0600

    feat: add-activity route

--- routes/activity_routes.py
+++ routes/activity_routes.py
@@ -3,6 +3,17 @@ from datetime import datetime
 from extensions import mongo 
 from services.carbon_service import calculate_carbon
 
-# Match the name you used in your app.register_blueprint
+
 activity_bp = Blueprint("activity_bp", __name__)
+
+@activity_bp.route("/add-activity", methods=["POST"])
+def add_activity():
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+
+    user_email = data.get("user_email")
+    activity_type = data.get("activity_type")
+    duration = data.get("duration_minutes")
+    data_used = data.get("data_used_mb")
 
